823d7fa6c0c9d3e191122a2d004f4185f73afeed
Cori Bradford <bradfcor@oregonstate.edu>
2021-11-11 14:10:26 -0800

    split into two classes

--- main.rb
+++ main.rb
@@ -1,12 +1,26 @@
 # Exercise 6
 
+class ParticipantArray
+	
+	def initialize(email_addresses)
+		@email_addresses = email_addresses
+	end
+
+	def generate_participant_users_from_email_string
+		return if @email_addresses.blank?
+		@email_addresses.split.uniq.map do |email_address|
+			User.create(email: email_address.downcase, password: Devise.friendly_token)
+		end
+	end
+
+end
+
 class LaunchDiscussionWorkflow
 
 	def initialize(discussion, host, participants_email_string)
 		@discussion = discussion
 		@host = host
-		@participants_email_string = participants_email_string
-		@participants = []
+		@participants = participants
 	end
   
 	# Expects @participants array to be filled with User objects
@@ -20,13 +34,6 @@ class LaunchDiscussionWorkflow
 			end
 		end
 	end
-  
-	def generate_participant_users_from_email_string
-		return if @participants_email_string.blank?
-		@participants_email_string.split.uniq.map do |email_address|
-			User.create(email: email_address.downcase, password: Devise.friendly_token)
-		end
-	end
 
 	# ...
 end
@@ -34,7 +41,8 @@ end
   
 discussion = Discussion.new(title: "fake", ...)
 host = User.find(42)
-participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
+email_addresses = "fake1@example.com\nfake2@example.com\nfake3@example.com"
+participants = ParticipantArray.new(email_addresses)
 
 workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
 workflow.generate_participant_users_from_email_string
